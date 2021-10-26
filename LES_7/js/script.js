faf4c677af6d988893b7e46ba168a5bc0f2f5169
epilar <epilar@ev-s3.kzn.21-school.ru>
2021-10-26 14:05:16 +0300

    les6-7_commit

---
+++ LES_7/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert("All is fine!");
+
+// const result = confirm("Are you OK?");
+// console.log(result);
+
+// const answer = prompt("Земля плоская?", "Нет!");
+// console.log(typeof(answer));
+
+const answer = [];
+
+answer[0] = prompt('Имя?', '');
+answer[1] = prompt('Фамилия?', '');
+answer[2] = prompt('Возраст?', '');
+
+document.write(answer);
+
+console.log(typeof(answer));
