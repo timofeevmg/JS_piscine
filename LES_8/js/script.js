422be52810499032a5aed41ff27b5956bebd0577
epilar <epilar@ev-s3.kzn.21-school.ru>
2021-10-27 11:06:55 +0300

    les8-12

---
+++ LES_8/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const category = 'auto';
+
+console.log(`https://fishki.net/${category}/`);
+
+const user = 'Maksim';
+
+alert(`Привет, ${user}!`);
---
+++ LES_9/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// console.log('arr' + ' - object');
+// console.log(4 + +'5');
+
+let incr = 10,
+	decr = 10;
+
+// incr++;
+// decr--;
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(2 * 3 == 6);
+console.log(2 * 3 == '6');
+console.log(2 * 3 === '6');
+console.log(2 * 3 === 6);
