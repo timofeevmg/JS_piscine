d70d4129ca5e9891f080b6b995197420be16ce7f
epilar <epilar@ev-n6.kzn.21-school.ru>
2021-10-31 14:38:49 +0300

    les28-30

--- LES_28/js/script.js
+++ LES_28/js/script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const box = document.getElementById('box'); // один элемент страницы, тк id уникален
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button'); // в btns получили не один элемент, а псевдомассив!
+
+console.log(btns[0]); // первый элемент псевдомассива HTMLCollection
+
+const circles = document.getElementsByClassName('circle'); // HTMLCollection
+
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+// console.log(hearts);
+hearts.forEach(item => {
+	console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
