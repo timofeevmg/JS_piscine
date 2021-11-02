a05c3a55e1aa719370aef1107a3db622b83ea49d
epilar <epilar@ev-n6.kzn.21-school.ru>
2021-11-02 13:13:17 +0300

    les_35_finish

--- LES_35/script.js
+++ LES_35/script.js
@@ -2,3 +2,14 @@
 
 const p = document.querySelectorAll('p');
 console.log(p);
+
+
+function loadScript(src) {
+	const script = document.createElement('script');
+	script.src = src;
+	script.async = false;// теперь выключили асинхронность и скрипты выполняться строго друг за другом
+	document.body.append(script);// добавили новый скрипт на страницу
+};
+
+loadScript("test.js");
+loadScript("some.js");
